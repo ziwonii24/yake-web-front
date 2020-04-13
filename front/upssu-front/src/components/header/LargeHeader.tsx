5bc734bfa275e2309a475bf91f73993026f9e3b9
ziwonii24 <ziwonii25@gmail.com>
2020-04-13 19:04:02 +0900

    large header frame & 전체 css, font

--- front/upssu-front/src/components/header/LargeHeader.tsx
+++ front/upssu-front/src/components/header/LargeHeader.tsx
@@ -1,9 +1,26 @@
 import React, { FunctionComponent } from 'react'
 
+import './style/Header.scss'
+
 const LargeHeader: FunctionComponent = () => {
 
     return (
-        <div>Large header</div>
+        <div className="template-lg">
+            <div className="user-box-lg">
+                <a>LOGIN</a>
+                &nbsp;|&nbsp;
+                <a>JOIN</a>
+            </div>
+
+            <div className="logo-box-lg">
+                YAKE
+            </div>
+
+            <div className="menu-box-lg">
+                <div className="col-menu">menu | menu | menu</div>
+                <div className="col-search">search input</div>
+            </div>
+        </div>
     )
 }
 
