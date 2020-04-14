ce5295407e9960313804a87d0de94b68e3dc5f26
ziwonii24 <ziwonii25@gmail.com>
2020-04-14 17:03:16 +0900

    add :: hot keyword skeleton

--- front/upssu-front/src/pages/MainPage.tsx
+++ front/upssu-front/src/pages/MainPage.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
 
 import InfoCarousel from '../components/carousel/InfoCarousel'
+import HotKeywordList from '../components/keyword/HotKeywordList'
 
 const MainPage = () => {
+
+    // user
+
     return (
         <div>
             <InfoCarousel />
+            <HotKeywordList />
+            {/* user && recommend products */}
+            {/* best products */}
+            {/* specific(age, gender) products */}
         </div>
     )
 }
